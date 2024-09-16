68e40d2a7d03ff9fba851df43eb6a08c8c954a38
shakhera <shakherakhanom024@gmail.com>
2024-09-16 17:57:32 +0600

    implement a simple footer

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -1,8 +1,25 @@
 const Footer = () => {
   return (
-    <div>
-      <h2>footer</h2>
-    </div>
+    <footer className="bg-gray-100  py-3">
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+          {/* Left section */}
+          <div>
+            <h2 className="text-xl font-bold text-gray-700">
+              Pharma Solutions
+            </h2>
+          </div>
+
+          {/* Right section */}
+          <div>
+            <p className="text-sm text-gray-700">
+              &copy; {new Date().getFullYear()} Pharma Solutions. All rights
+              reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
